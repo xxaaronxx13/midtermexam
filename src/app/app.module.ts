7e96cac24881e23be587320a2b87df848dfbf345
PejayZeta <pejyboyzeta@gmail.com>
2021-10-27 21:44:36 +0700

    commit for main/master branch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,49 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactsNavigationComponent } from './contacts-navigation/contacts-navigation.component';
+import { RouterModule, Routes } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
+import { ProfileComponent } from './profile/profile.component';
+import { GalleryComponent } from './gallery/gallery.component';
+
+const routes: Routes = [
+    {
+    path:'contacts-navigation', component: ContactsNavigationComponent,
+    children:[
+    {path:'profile', component: ProfileComponent},
+    {path:'gallery', component: GalleryComponent},
+      ]
+
+    },
+    {path:'**', redirectTo:'contacts-navigation'}
+  
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactsNavigationComponent,
+    ProfileComponent,
+    GalleryComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(
+      routes
+    ),
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/contacts-navigation/contacts-navigation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contacts-navigation',
+  templateUrl: './contacts-navigation.component.html',
+  styleUrls: ['./contacts-navigation.component.css']
+})
+export class ContactsNavigationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
